b43674e3e29fd337cbc1e65184e8103deabf7a63
namontoya <161357317+namontoya@users.noreply.github.com>
2024-02-26 13:09:27 -0500

    Add files via upload

---
+++ EJERCICIO_3_POO.py
@@ -0,0 +1,16 @@
+#Ejercicio Propuesto No 12
+
+horas_trabajadas = 48
+valor_hora = 5000
+salario_bruto = valor_hora*horas_trabajadas
+retencion = salario_bruto*(12.5/100)
+salario_neto = salario_bruto - retencion
+
+print("Las horas trabajadas son:", horas_trabajadas)
+print("El valor de la hora es:", valor_hora)
+print("EL SALARIO BRUTO ES:", salario_bruto)
+print("LA RETENCION EN LA FUENTE ES:", retencion)
+print("EL SALARIO NETO ES:", salario_neto)
+
+
+
